478e7636ab7063bc81c0a59296c47ceeb7fe64b0
Nathan Pierce <norsegaud@gmail.com>
2026-07-27 19:26:01 -0400

    fix: mock wasm assets in Jest for sql.js client imports

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,10 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   modulePaths: ['node_modules', '<rootDir>'],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/']
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  // sql.js client imports a .wasm asset (handled by Webpack in the app).
+  // Jest cannot load wasm; map it to a string stub.
+  moduleNameMapper: {
+    '\\.wasm$': '<rootDir>/src/__mocks__/wasmMock.js'
+  }
 };
---
+++ src/__mocks__/wasmMock.js
@@ -0,0 +1,2 @@
+// Jest stand-in for Webpack's .wasm file-loader / asset modules.
+module.exports = 'mock.wasm';
